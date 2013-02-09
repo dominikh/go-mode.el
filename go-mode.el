c97dacc0ecc2b5ba24e09ff3db750409e48624be
Dominik Honnef <dominikh@fork-bomb.org>
2013-02-09 06:13:37 +0100

    do not change the indentation of multiline comments

--- go-mode.el
+++ go-mode.el
@@ -196,28 +196,30 @@ built-ins, functions, and some types.")
 
 (defun go-mode-indent-line ()
   (interactive)
-  (let ((indent (go-indentation-at-point (point)))
-        shift-amt
-        end
-        (pos (- (point-max) (point)))
-        (beg (progn (beginning-of-line) (point))))
-    (back-to-indentation)
-    (if (looking-at "case .+:\\|default:")
-        (setq indent (- indent tab-width)))
-    (beginning-of-line)
-    (if (and (looking-at "^[[:space:]]*[[:word:]]+:\\([[:space:]]*/.+\\)?$")
-             (not (looking-at "^[[:space:]]*default:")))
-        (setq indent 0))
-    (skip-chars-forward " \t")
-    (setq shift-amt (- indent (current-column)))
-    (if (zerop shift-amt)
-        nil
-      (delete-region beg (point))
-      (indent-to indent))
-    ;; If initial point was within line's indentation,
-    ;; position after the indentation.  Else stay at same point in text.
-    (if (> (- (point-max) pos) (point))
-        (goto-char (- (point-max) pos)))))
+  (beginning-of-line)
+  (if (not (go-in-comment-p))
+      (let ((indent (go-indentation-at-point (point)))
+            shift-amt
+            end
+            (pos (- (point-max) (point)))
+            (beg (progn (beginning-of-line) (point))))
+        (back-to-indentation)
+        (if (looking-at "case .+:\\|default:")
+            (setq indent (- indent tab-width)))
+        (beginning-of-line)
+        (if (and (looking-at "^[[:space:]]*[[:word:]]+:\\([[:space:]]*/.+\\)?$")
+                 (not (looking-at "^[[:space:]]*default:")))
+            (setq indent 0))
+        (skip-chars-forward " \t")
+        (setq shift-amt (- indent (current-column)))
+        (if (zerop shift-amt)
+            nil
+          (delete-region beg (point))
+          (indent-to indent))
+        ;; If initial point was within line's indentation,
+        ;; position after the indentation.  Else stay at same point in text.
+        (if (> (- (point-max) pos) (point))
+            (goto-char (- (point-max) pos))))))
 
 (defun go-beginning-of-defun (&optional count)
   (unless count (setq count 1))
---
+++ indentation_tests/multiline_comment.go
@@ -0,0 +1,11 @@
+package multilinecomment
+
+/* Some comment here
+	with my very own
+		indentation as it pleases me */
+
+func main() {
+	if true {
+		// code
+	}
+}
