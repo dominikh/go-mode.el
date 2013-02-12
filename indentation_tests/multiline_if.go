dominikh/go-mode.el

commit 95c9519fb9e010df8e9aa91d5925a84a1f97d424

when searching for anchor for indentation, do not limit search to one line
Closes gh-11

go-mode.el
@@ -174,14 +174,11 @@ built-ins, functions, and some types.")
                   (current-indentation)
                 (+ (current-indentation) tab-width))
             (go--backward-irrelevant)
-            (if (go-previous-line-has-dangling-op-p)
-                (setq outindent tab-width))
             (setq line-begin (line-beginning-position))
             (while (and
                     (not (bobp))
-                    (> (point) line-begin)
                     (>= (go-paren-level) start-nesting))
-              (if (= 0 (skip-chars-backward "^[]{}()" line-begin))
+              (if (= 0 (skip-chars-backward "^[]{}()"))
                   (backward-char))
               (if (go-in-string-p)
                   (progn

commit 72cf1263e0ba863e7f434d10378a1b4dec2bb9fa

fix indentation for multi-line if expressions
This fixes a regression introduced by
95c9519fb9e010df8e9aa91d5925a84a1f97d424

go-mode.el
@@ -193,6 +193,8 @@ built-ins, functions, and some types.")
               (if (< (go-paren-level) start-nesting)
                   (if (go-previous-line-has-opening-parenthesis-p)
                       (current-indentation)
+                    (if (go-previous-line-has-dangling-op-p)
+                        (setq outindent tab-width))
                     (+ (current-indentation) tab-width (- outindent)))
                 (- (current-indentation) outindent)))))))))
 

indentation_tests/multiline_if.go
@@ -1,0 +1,5 @@
+if realLength == -1 &&
+	!chunked(t.TransferEncoding) &&
+	bodyAllowedForStatus(t.StatusCode) {
+	t.Close = true
+}