d7c48cbca8334b576ff32c7e34de552e717d4020
Dominik Honnef <dominikh@fork-bomb.org>
2013-02-20 08:51:48 +0100

    give in and require cl at runtime

--- go-mode.el
+++ go-mode.el
@@ -4,9 +4,7 @@
 ;; Use of this source code is governed by a BSD-style
 ;; license that can be found in the LICENSE file.
 
-(eval-when-compile
-  (require 'cl))
-
+(require 'cl)
 (require 'diff-mode)
 (require 'ffap)
 (require 'find-lisp)
