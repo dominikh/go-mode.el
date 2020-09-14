4db839a5cc0fa9fe0b6d4d4ae8449af3853e48c7
Muir Manders <muir@mnd.rs>
2020-09-13 21:08:28 -0700

    Support basic indenting in multiline comments.
    This commit changes a few things to get indenting working in multiline
    comments:
    
    1. Make go-indentation-at-point return something when inside multiline
       comments. It basically guesses an indent based on the previous line
       or start of comment, along with a special case to line up all the
       "*" if you are making comment art. I didn't think it was worth it
       to support all of gofmt's multiline comment behavior.
    
    2. Don't ignore comments in go-mode-indent-line.
    
    3. Tweak go-mode-indent-line to not uses tabs when indenting beyond
       the "/*" in multiline comments. If you use tabs, gofmt ends up
       changing things around in unexpected ways.
    
    4. Tweak our electric indent function to trigger completion after
       newline within comments, and after typing the closing "*/".
    
    Fixes #369.

--- test/testdata/indentation_tests/multiline_comment.go
+++ test/testdata/indentation_tests/multiline_comment.go
@@ -9,3 +9,46 @@ func main() {
 		// code
 	}
 }
+
+func _() {
+	/*   foo
+	 * bar
+	 */
+
+	/* abc
+	123
+	   def
+	     lol
+	*/
+
+	/*
+	   abc
+	     - def
+	*/
+
+	/*
+	   hello
+	there */
+
+	/*
+	   hello
+	   there */
+
+	/*
+	   foo
+	*/
+
+	/*
+	  foo
+	*/
+
+	/*
+	 foo
+	*/
+
+	/* foo
+	asd
+   asd
+	   asd
+	*/
+}
